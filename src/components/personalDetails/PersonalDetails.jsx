a6796bb7c5e1b5ba050d97783b0283e9ef0855b2
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-08-02 15:51:13 +0200

    FIX: fix personal details functionality

--- src/components/personalDetails/PersonalDetails.jsx
+++ src/components/personalDetails/PersonalDetails.jsx
@@ -1,15 +1,17 @@
 import InputGenerator from "../InputGenerator";
+import data from '../../assets/savedData';
 
-function PersonalDetails( { fullName, email, phoneNumber, address, onChange } ) {
+function PersonalDetails({ fullName, email, phoneNumber, address, onChange, onFocusOut } ) {
     return (
-        <form className='personal-info'>
+        <>
             <h1>Personal Details</h1>
-
-            <InputGenerator labelText='Full name' value={fullName} type='text' dataIndex='fullName' onChange={onChange} />
-            <InputGenerator labelText='Email' value={email} type='email' dataIndex='email' onChange={onChange} />
-            <InputGenerator labelText='Phone number' value={phoneNumber} type='text' dataIndex='phoneNumber' onChange={onChange} />
-            <InputGenerator labelText='Address' value={address} type='text' dataIndex='address' onChange={onChange} />
-        </form>
+            <form className='personal-info'>
+                <InputGenerator labelText='Full name' value={fullName} type='text' placeholder='Josephine Meyers' dataIndex='fullName' onChange={onChange} onFocusOut={onFocusOut} />
+                <InputGenerator labelText='Email' value={email} type='email' placeholder='josephine.meyers@gmail.com' dataIndex='email' onChange={onChange} onFocusOut={onFocusOut} />
+                <InputGenerator labelText='Phone number' value={phoneNumber} type='text' placeholder='+44 123 456 789' dataIndex='phoneNumber' onChange={onChange} onFocusOut={onFocusOut} />
+                <InputGenerator labelText='Address' value={address} type='text' placeholder='London, UK' dataIndex='address' onChange={onChange} onFocusOut={onFocusOut} />
+            </form>
+        </>
     )
 }
 
