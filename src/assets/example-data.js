383831fbb94cdad29bbf5c6822d21612e648f334
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-08-03 23:29:54 +0200

    ADD: clear and load template functionality

--- src/assets/example-data.js
+++ src/assets/example-data.js
@@ -1,7 +1,7 @@
 import uniqid from 'uniqid';
 
 const exampleData = {
-    personalInfo: {
+    personalDetails: {
         fullName: "Josephine Meyers",
         email: "josephine.meyers@mail.co.uk",
         phoneNumber: "+44 3245 5521 5521",
@@ -15,9 +15,8 @@ const exampleData = {
             location: "New York City, US",
             startDate: "08/2020",
             endDate: "present",
-            isCollapsed: true,
-            isHidden: false,
-            id: uniqid(),
+            hidden: false,
+            key: uniqid(),
         },
         {
             degree: "Master's Degree in Math",
@@ -25,9 +24,8 @@ const exampleData = {
             location: "New York City, US",
             startDate: "08/2020",
             endDate: "present",
-            isCollapsed: true,
-            isHidden: true,
-            id: uniqid(),
+            hidden: true,
+            key: uniqid(),
         },
     ],
 
@@ -40,9 +38,8 @@ const exampleData = {
                 "Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android",
             startDate: "08/2020",
             endDate: "present",
-            isCollapsed: true,
-            isHidden: false,
-            id: uniqid(),
+            hidden: false,
+            key: uniqid(),
         },
         {
             companyName: "Black Mesa Labs",
@@ -52,9 +49,8 @@ const exampleData = {
                 "Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.",
             startDate: "04/2018",
             endDate: "02/2019",
-            isCollapsed: true,
-            isHidden: false,
-            id: uniqid(),
+            hidden: false,
+            key: uniqid(),
         },
     ],
 };
--- src/assets/savedData.js
+++ src/assets/savedData.js
@@ -1,20 +1,11 @@
 import { getStorage, setStorage } from "./localStorage";
 import uniqid from 'uniqid';
+import exampleData from "./example-data";
 
 let savedData;
 
 if (getStorage('savedData') === null) {
-    savedData = {
-        personalDetails: {
-            fullName: '',
-            email: '',
-            phoneNumber: '',
-            address: ''
-        },
-        education: [],
-        experience: []
-    }
-
+    savedData = exampleData;
     setStorage('savedData', savedData);
 } else {
     savedData = getStorage('savedData');
@@ -28,49 +19,96 @@ export default function data() {
     const savePersonalInfo = (data) => {
         savedData.personalDetails = data;
         setStorage('savedData', savedData);
-    }
+    };
 
     const findEducationIndex = (key) => {
         return savedData.education.findIndex((element) => element.key === key);
-    }
+    };
 
     const editEducation = (data) => {
         const educationIndex = findEducationIndex(data.key);
         savedData.education[educationIndex] = data;
         setStorage('savedData', savedData);
-    }
+    };
 
     const removeEducation = (key) => {
         const educationIndex = findEducationIndex(key);
         savedData.education.splice(educationIndex, 1);
         setStorage('savedData', savedData);
-    }
+    };
+
+    const hideEducation = (key) => {
+        const educationIndex = findEducationIndex(key);
+        savedData.education[educationIndex].hidden = !savedData.education[educationIndex].hidden;
+        setStorage('savedData', savedData);
+    };
 
     const addEducation = ({schoolName = '', degree = '', location = '', startDate = '', endDate = ''}) => {
-        savedData.education.push({ schoolName, degree, location, startDate, endDate, key: uniqid()});
+        savedData.education.push({ schoolName, degree, location, startDate, endDate, hidden: false, key: uniqid()});
         setStorage('savedData', savedData);
-    }
+    };
 
     const findExperienceIndex = (key) => {
         return savedData.experience.findIndex((element) => element.key === key);
-    }
+    };
 
     const editExperience = (data) => {
         const experienceIndex = findExperienceIndex(data.key);
         savedData.experience[experienceIndex] = data;
         setStorage('savedData', savedData);
-    }
+    };
 
     const removeExperience = (key) => {
         const experienceIndex = findExperienceIndex(key);
         savedData.experience.splice(experienceIndex, 1);
         setStorage('savedData', savedData);
-    }
+    };
 
-    const addExperience = ({ companyName = '', positionTitle = '', location = '', startDate = '', endDate = '', description = '' }) => {
-        savedData.experience.push({ companyName, positionTitle, location, startDate, endDate, description, key: uniqid() });
+    const hideExperience = (key) => {
+        const experienceIndex = findExperienceIndex(key);
+        savedData.experience[experienceIndex].hidden = !savedData.experience[experienceIndex].hidden;
         setStorage('savedData', savedData);
-    }
+    };
 
-    return { personalInfo, educationList, experienceList, savePersonalInfo, editEducation, removeEducation, addEducation, editExperience, removeExperience, addExperience };
+    const addExperience = ({ companyName = '', positionTitle = '', location = '', startDate = '', endDate = '', description = ''}) => {
+        savedData.experience.push({ companyName, positionTitle, location, startDate, endDate, description, hidden: false, key: uniqid() });
+        setStorage('savedData', savedData);
+    };
+
+    const clearData = () => {
+        savedData = {
+            personalDetails: {
+                fullName: '',
+                email: '',
+                phoneNumber: '',
+                address: ''
+            },
+            education: [],
+            experience: []
+        }
+
+        setStorage('savedData', savedData);
+    };
+
+    const loadData = () => {
+        savedData = exampleData;
+        setStorage('savedData', savedData);
+    };
+
+    return { 
+        personalInfo,
+        educationList,
+        experienceList,
+        savePersonalInfo,
+        editEducation,
+        removeEducation,
+        hideEducation,
+        addEducation,
+        editExperience,
+        removeExperience,
+        hideExperience,
+        addExperience,
+        clearData,
+        loadData,
+    };
 }
